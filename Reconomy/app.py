1453114ce2ca0289b1600d08bd2832a282ea089c
Seif-S <Seif-diya@hotmail.com>
2024-08-08 10:09:02 +0200

    Fixed csv writing empty string for username and placed js code in a seperate file

--- Reconomy/app.py
+++ Reconomy/app.py
@@ -5,19 +5,18 @@ import datetime
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
-def index():
-    username = request.args.get('todo')
-    if username != '' or username != None:
-        try:
-            currentTime = datetime.datetime.today()
+async def index():
+    try:
+        username = request.args.get('todo')
+        currentTime = datetime.datetime.today()
+        if username and username.strip():
             data = {'Name':username, 'Time':currentTime}
             with open('timeReport.csv','at', newline='') as file:
                 fieldname = data.keys()
                 csvWrite = csv.DictWriter(file, fieldnames=fieldname)
                 csvWrite.writerow(data)
-            return render_template('index.html')
-        except Exception as error:
-            return repr(error)
+    except Exception as error:
+        return repr(error)
     return render_template('index.html')
 
 if __name__ == '__main__':
